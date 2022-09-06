f78477ead61f5974da2c235b8ace1baa972f2c9b
LazyRiver18 <me.sierzega@gmail.com>
2022-09-05 22:49:24 -0400

    cleane dup the code and added comments

--- annotator.py
+++ annotator.py
@@ -1,27 +1,20 @@
 
-# import OS module
-from fileinput import close
 import os
 from bs4 import BeautifulSoup
 from PIL import Image, ImageDraw as D
  
-# Get the list of all files and directories
-path = "C://Users//purpl//OneDrive//Documents//William_and_Mary//Software_Engineering//CS435IntroAssignment//Programming-Assignment-Data//"
+# Get the list of all files and directories from the user
+path = input("Please enter the path of the set of xml/png files: ")
 files_list = os.listdir(path)
- 
-print("Files and directories in '", path, "' :")
- 
-# prints all files
-print(files_list)
 
+#for each xml file, sets the corresponding png file to annotate
 for i in range(len(files_list)):
     if files_list[i].endswith(".xml"):
         cur_xml = files_list[i]
         counter = i+1
         cur_image = cur_xml[0:-3] + "png"
 
-        
-        #cur_image = files_list[0]
+        #retreiving the files from the user's path
         file = os.path.join(path, cur_xml)
         image_file = os.path.join(path, cur_image)
 
@@ -29,13 +22,16 @@ for i in range(len(files_list)):
         with open(file, 'r', encoding = 'UTF-8') as f:
             xml_file = f.read()
 
-            # print(lines)
+        #parses the xml file for an easier to manipulate hierarchy
         soup = BeautifulSoup(xml_file, "xml")
 
+        #puts all the node tags into a list
         nodes = soup.find_all('node')
 
         img = Image.open(image_file)
         for n in nodes:
+
+            #finds and puts boxes around the nodes with no children (involves parsing the bounds attribute string)
             if(len(n.contents) == 0):
                 bounds_str = n["bounds"]
                 comma = bounds_str.index(",")
@@ -48,7 +44,11 @@ for i in range(len(files_list)):
           
                 draw=D.Draw(img)
                 draw.rectangle([(int(x1),int(y1)),(int(x2),int(y2))],outline="yellow", width = 5)
-        img.show()
+
+        # used for debugging purposes
+        # img.show() 
+
+        #saves each annotated png file
         img = img.save(cur_xml[0:-4] + "Annotated.png")
 
     
